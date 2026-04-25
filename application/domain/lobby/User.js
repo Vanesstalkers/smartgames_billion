378df899f909d39fc9f1552817cb52234376cf92
VanesStalkers <ivan.mironov@bk.ru>
2026-04-25 23:01:56 +0300

    (+) initial user lobbyConfigs

--- application/domain/lobby/User.js
+++ application/domain/lobby/User.js
@@ -10,6 +10,23 @@
       broadcastableFields: this.broadcastableFields(),
     });
 
+    this.set({
+      lobbyConfigs: {
+        pinnedItems: {
+          game: true,
+        },
+        billion: {
+          gameCode: config.smartgames.appCode,
+          gameConfig: 'main',
+          gameRoundLimit: 40,
+          gameTimer: 60,
+          gameType: 'trainer',
+          maxPlayersInGame: { max: 8, min: 1, val: 8 },
+          minPlayersInGame: { max: 8, min: 1, val: 1 },
+        },
+      },
+    });
+
     await super.enterLobby({ sessionId, lobbyId });
   }
 });
